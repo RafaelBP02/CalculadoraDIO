c0f7b3f769c61578fb38993c5ea9d79db368ed8d
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-04-29 18:13:13 -0300

    Feat: Implementa metodo de soma e testes

--- TesteCalculadoraDIO/UnitTest1.cs
+++ TesteCalculadoraDIO/UnitTest1.cs
@@ -1,11 +1,19 @@
+using CalculadoraDIO;
+
 namespace TesteCalculadoraDIO
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        private Calculadora _calculadora = new Calculadora();
 
+        [Theory]
+        [InlineData(1, 4, 5)]
+        [InlineData(17, 14, 31)]
+        [InlineData(10003, 11007, 21010)]
+        [InlineData(int.MaxValue, int.MinValue, -1)]
+        public void Soma_DoisNumeros_RetornaResultadoCorreto(int x, int y, int valorEsperado)
+        {
+            Assert.Equal(valorEsperado, _calculadora.Soma(x, y));  
         }
     }
 }
